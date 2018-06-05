51977a0037e49bd41ebb61c379b5d5950df770f7
Thor Galle <thor.galle@student.kuleuven.be>
2018-06-05 23:54:49 +0200

    Adds surrounding sentence extractor, works fairly well

--- content/voc-adder.js
+++ content/voc-adder.js
@@ -43,12 +43,59 @@ document.addEventListener('load', () => {
     });
 })
 
-document.addEventListener('selectionchange', () => {
+/**
+ * @returns the sentence if found, null otherwise
+ */
+function getSurroundingSentence() {
+  if (!document.selection) { // anti IE 
+      const word = getSelectedText();
+      let selectionNode = window.getSelection().anchorNode;
+
+      const escapeRegExp = function(str) {
+        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+      }
+      const regWord = escapeRegExp(word);
+      const regWordDecapitated = regWord.substring(1);
+      // could probably be improved.
+      // Sentence starts with a capital letter and ends with a period. 
+      // (but what if a capital name enters somewhere?...) --> lookbehind could be a solution, but not supported
+      // for the moment: [^\s] : no space before the capital letter
+      // to be tolerant: sentence can also start at the end of another sentence
+      let sentenceReg = new RegExp(`(\\.\\s{1,3}|[^\\s][A-Z])[^\\.]*(${regWord}|${regWordDecapitated})[^\\.]*\\.`);
+
+
+      let sentence = selectionNode.textContent;
+      let hopCount = 0;
+      let match = sentenceReg.exec(sentence);
+      while (sentence && !match && hopCount < 5) {
+        selectionNode = selectionNode.parentElement;
+        sentence = selectionNode.textContent;
+        match = sentenceReg.exec(sentence);
+        hopCount++;
+      }
+
+      if (match)  {
+          // might have matched too much. A second pass in order to keep the regex above cleaner...
+          const rawMatch = match[0];
+          let fineMatch = /\.\s{1,3}(.*)/.exec(rawMatch);
+          if (fineMatch) {
+            return fineMatch[1];
+          } else {
+            return rawMatch
+          }
+      } else {
+        return null;
+      }
+  }
+}
+
+document.addEventListener('selectionchange', ()=> {
     const text = getSelectedText();
     if (text) {
         chrome.runtime.sendMessage({
             type: 'selection',
-            selection: text
+            selection: text,
+            sentence: getSurroundingSentence()
         });
     }
 })
