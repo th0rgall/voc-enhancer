44351ff1ddd21a2993585d5d5bf0fdcb66709e1f
Thor Galle <thor.galle@student.kuleuven.be>
2018-06-23 12:13:34 +0200

    Fixes definition translation injector

--- content/definition-injector.js
+++ content/definition-injector.js
@@ -1,7 +1,7 @@
 
 function initialize() {
     // insert translation element
-    render('#pageContent', {observe: true, firstPass: true}, (wordPage) => {
+    render('#pageContent', {observe: true, firstPass: true}, (pageContent) => {
         // get word
         let wordEl = document.querySelector('div[data-word]');
         if (wordEl) {
