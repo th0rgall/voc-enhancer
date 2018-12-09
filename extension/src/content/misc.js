0a08f68f2d06d541fb8dccf9ae7ad5dd4000d687
Thor Galle <thor.galle@student.kuleuven.be>
2018-12-09 16:48:35 +0200

    Restructure

---
+++ extension/src/content/misc.js
@@ -0,0 +1,31 @@
+if (document.readyState === "complete" ) { 
+    initMisc();
+} else {
+    window.addEventListener('load', initMisc);
+}
+
+function initMisc() {
+    insertListLink();
+}
+
+/**
+ * 
+ */
+function insertListLink() {
+    let el = document.querySelector("header.page-header ul");
+    if (el) {
+        const li = document.createElement('li');
+        li.classList.add('listsTab');
+        li.title = "See your Vocabulary lists";
+        li.setAttribute('unselectable', 'on');
+        li.style = "-moz-user-select: none;";
+
+        const a = document.createElement('a');
+        a.href = '/account/lists';
+        const span = document.createElement('span');
+        span.appendChild(document.createTextNode('MY LISTS'));
+        a.appendChild(span);
+        li.appendChild(a);
+        el.appendChild(li);
+    }
+}
