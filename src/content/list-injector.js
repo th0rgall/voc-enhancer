3afe9716739ae5a6aee90991568aa3e4296c91c3
Thor Galle <thor.galle@student.kuleuven.be>
2019-01-11 15:12:04 +0200

    Rename translate + disable invalid links

--- src/content/list-injector.js
+++ src/content/list-injector.js
@@ -2,10 +2,17 @@ function initialize() {
     // insert translation element
     render('#wordlist', {observe: false}, (wordlist) => {
         const lis = wordlist.querySelectorAll('li');
+        // insert service links
         for (let i = 0; i < lis.length; i++) {
             const current = lis[i];
             const word = current.getAttribute('word');
             current.insertBefore(createLinks(word), current.children[1]);
+
+            // disable source links that won't work anyway
+            let link = current.querySelector("a.source");
+            if (link && link.getAttribute("href") === "https://corpus.vocabulary.com/go/0") {
+                link.classList.add('ve-source-link');
+            } 
         };
     });
 }
