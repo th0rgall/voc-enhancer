580eae18bc921c8b9e1ab53f66b12bcfb137164e
Thor Galle <thor.galle@student.kuleuven.be>
2019-01-19 19:03:02 +0200

    Extracts words again after easy data property was removed from vocab.com

--- src/content/quiz-injector.js
+++ src/content/quiz-injector.js
@@ -2,20 +2,60 @@
 function initialize() {
     render('.questionPane > div:last-child .blurb-container', {observe: true, rootSelector: '.questionPane'}, (descriptionParent) => {
 
-        // get word
-        // Jan 17, 19: word not anymore directly in page
-        let word = document.querySelector('.questionPane > div:last-child .instructions strong');
-        if (!word) {
-            // try again, hidden-type question
-            word = document.querySelector('.questionPane > div:last-child .sentence.complete strong');
+        function getWord() {
+            // get word
+            // Jan 17, 19: word not anymore directly in page
+            // TODO: first extract last-child, then continue on that...
+            let lastChild = document.querySelector('.questionPane > div:last-child');
+            let word;
+            if (lastChild) {
+                // Question type 1: "The meaning of <word> is ..."
+                word = lastChild.querySelector('.instructions strong');
+                if (word) {
+                    return word.innerText;
+                } else {
+                    // Question type 2: hidden answer + type something question
+                    word = lastChild.querySelector('.sentence.complete strong');
+                    if (word) {
+                        return word.innerText;
+                    } else {
+                       // Question type 3: hidden answer + multiple choice question
+                       word = lastChild.querySelector('.choices .correct .lookup');
+                       if (word && word.getAttribute('word')) {
+                           return word.getAttribute('word');
+                       } else {
+                          // Question type 4: choose the best picture for ... possible
+                        // TODO: check
+                        word = lastChild.querySelector('.word .wrapper');
+                        if (word && word.childNodes.length > 1) {
+                            return word.childNodes[1].textContent.trim();
+                        } else {
+                            // Question type 5: "Which would be considered... ?" 
+                            word = lastChild.querySelector('.sentence strong');
+                            if (word) {
+                                return word.innerText;
+                            } else {
+                                return null;
+                            }
+                         }
+                       }
+                    }
+                }
+            } else {
+                throw "Could not access last slide";
+            }
         }
-        if (word) word = word.innerText;
-        let quizInjection = document.createElement('div');
-        quizInjection.classList.add('ve-quiz-injection');
-        quizInjection.appendChild(createTranslation(word));
-        quizInjection.appendChild(createLinks(word));
 
-        descriptionParent.children[0].insertBefore(
-            quizInjection, descriptionParent.querySelector('.more'));
+        let word = getWord();
+        if (word) {
+            let quizInjection = document.createElement('div');
+            quizInjection.classList.add('ve-quiz-injection');
+            quizInjection.appendChild(createTranslation(word));
+            quizInjection.appendChild(createLinks(word));
+            descriptionParent.children[0].insertBefore(
+                quizInjection, descriptionParent.querySelector('.more')); 
+        } else {
+            throw "Could not find word from page";
+        }
         });
 }
