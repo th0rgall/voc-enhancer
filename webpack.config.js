770e50e0ad1c4b98d477bf7004550a63316c5e73
Thor Galle <thorgalle@gmail.com>
2019-04-02 02:14:13 +0300

    Enable a production build w/ webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ const FileManagerPlugin = require('filemanager-webpack-plugin');
 // const { EnvironmentPlugin } = require('webpack');
 // const log = require('webpack-log')({ name: 'wds' });
 const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
-const production = false;
 
 const copyFromSourceFiles = [
   "options/options.html",
@@ -30,107 +29,112 @@ const distributeFiles = [
 
 const distributeGlobs = distributeFiles.map(f => f.endsWith('/') ? f + "*" : f);
 
+// export function to access mode
+module.exports = (env, argv) => {
+  
+  const production = argv.mode === 'production';
+  // fm plugin delete files
+  const deleteFiles = distributeFiles.reduce((acc, of) => {
+    const base = 'dist/' + of;
+    return of.endsWith('.js') ? [...acc, base, base + '.map'] : [...acc, base]
+  }, []);
 
-module.exports = {
-  entry: {
-    ...entry('api/translate'), // todo: intergrateinto background
-    ...entry('api/store'),
-    ...entry('common'),
-    ...entry('background'),
-    ...entry('options/options'),
-    ...entryContentScripts()
-  },
-  target: 'web',
-  context: path.resolve(__dirname, 'src'),
-  devtool: 'source-map',
-  output: {
-    //filename: 'vocabulary-api.js',
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  },
-  resolve: {
-      aliasFields: ['browser'],
-  },
-  node: {
-    "voc-dom": "empty",
-    "voc-http": false
-  },
-  module: {
-    // rules: [
-    //   {
-    //     test: /\.js$/,
-    //     exclude: /node_modules/,
-    //     use: 'babel-loader'
-    //   }
-    // ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-        'process.env.COMPILE_ENV': JSON.stringify('webpack'),
-        'process.env.DEBUG': JSON.stringify('true')
-    }),
-    new CleanPlugin(),
-    // copy plugin copies from src to build
-    new CopyPlugin([
-      ...copy_same_arr.apply(null, copyFromSourceFiles),
-      {
-        from: 'manifest.json',
-        to: 'chrome/manifest.json',
-        transform: transformManifest('chrome')
-      },
-      {
-        from: 'manifest.json',
-        to: 'firefox/manifest.json',
-        transform: transformManifest('firefox')
-      }
-    ]),
-    // fm plugin manages all dirs. Used here for the build dir.
-    new FileManagerPlugin({
-      onEnd: [
+  return {
+    entry: {
+      ...entry('api/translate'), // todo: intergrateinto background
+      ...entry('api/store'),
+      ...entry('common'),
+      ...entry('background'),
+      ...entry('options/options'),
+      ...entryContentScripts()
+    },
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    devtool: 'source-map',
+    output: {
+      //filename: 'vocabulary-api.js',
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+    resolve: {
+        aliasFields: ['browser'],
+    },
+    node: {
+      "voc-dom": "empty",
+      "voc-http": false
+    },
+    module: {
+      // rules: [
+      //   {
+      //     test: /\.js$/,
+      //     exclude: /node_modules/,
+      //     use: 'babel-loader'
+      //   }
+      // ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+          'process.env.COMPILE_ENV': JSON.stringify('webpack'),
+          'process.env.DEBUG': JSON.stringify('true')
+      }),
+      new CleanPlugin(),
+      // copy plugin copies from src to build
+      new CopyPlugin([
+        ...copy_same_arr.apply(null, copyFromSourceFiles),
         {
-          // needs to be with globs in 1 source, otherwise folder structure is not retained...
-          copy: 
-            [{
-              source: 'dist/{' + distributeGlobs.join() + '}',
-              destination: 'dist/chrome/'
-          }, {
-              source: 'dist/{' + distributeGlobs.join() + '}',
-              destination: 'dist/firefox/'
-          }]
+          from: 'manifest.json',
+          to: 'chrome/manifest.json',
+          transform: transformManifest('chrome')
         },
-        !production && {
-          delete: [
-            ...distributeFiles.reduce((acc, of) => {
-              const base = 'dist/' + of;
-              return of.endsWith('.js') ? [...acc, base, base + '.map'] : [...acc, base]
-            }, [])
-          ]
-        },
-        production && {
-          delete: [
-            'dist/**/*.js.map',
-            ...outFiles
-          ],
-          archive: [
-            {
-              source: 'dist/chrome',
-              destination: `dist/voc-enhancer-chrome-${version}.zip`
-            },
-            {
-              source: 'dist/firefox',
-              destination: `dist/voc-enhancer-firefox-${version}.zip`
-            }
-          ]
+        {
+          from: 'manifest.json',
+          to: 'firefox/manifest.json',
+          transform: transformManifest('firefox')
         }
-      ]
-    })//,
-    // new ChromeExtensionReloader({
-    //   entries: { 
-    //     contentScript: ['common', 'api/translate', ...entryContentNames()],
-    //     background: 'background'
-    //   }
-    // })
-]
+      ]),
+      // fm plugin manages all dirs. Used here for the build dir.
+      new FileManagerPlugin({
+        onEnd: [
+          {
+            // needs to be with globs in 1 source, otherwise folder structure is not retained...
+            copy: 
+              [{
+                source: 'dist/{' + distributeGlobs.join() + '}',
+                destination: 'dist/chrome/'
+            }, {
+                source: 'dist/{' + distributeGlobs.join() + '}',
+                destination: 'dist/firefox/'
+            }]
+          },
+          !production && {
+            delete: deleteFiles
+          },
+          production && {
+            delete: [
+              'dist/**/*.js.map',
+              ...deleteFiles
+            ],
+            archive: [
+              {
+                source: 'dist/chrome',
+                destination: `dist/voc-enhancer-chrome-${pkg.version}.zip`
+              },
+              {
+                source: 'dist/firefox',
+                destination: `dist/voc-enhancer-firefox-${pkg.version}.zip`
+              }
+            ]
+          }
+        ]
+      })//,
+      // new ChromeExtensionReloader({
+      //   entries: { 
+      //     contentScript: ['common', 'api/translate', ...entryContentNames()],
+      //     background: 'background'
+      //   }
+      // })
+  ]
+  }
 };
 
 /** read titles of content file scripts
@@ -188,6 +192,15 @@ function transformManifest (browser) {
 
     if (browser === 'chrome') {
       manifest["background"]["persistent"] = true;
+      manifest["options_ui"]["chrome_style"] = true;
+    } else if (browser === 'firefox') {
+      manifest["browser_specific_settings"] = {
+          "gecko": {
+              "id": "vocenhancer@extension.org",
+              "strict_min_version": "42.0"
+          }
+      };
+      manifest["options_ui"]["browser_style"] = true;
     }
 
     manifest.version = pkg.version;
