2d92543e935b2ffb92b6de673219f41d5248add6
Thor Galle <thorgalle@gmail.com>
2019-06-02 00:09:35 +0300

    Initial source link collapse implementation

--- src/content/list-injector.js
+++ src/content/list-injector.js
@@ -15,7 +15,44 @@ function initialize() {
     // insert external service links
     forEachLi((current, word) => {
         // does a lot of things repetitively: cache insite this method
-        createLinks(word).then(result => current.insertBefore(result, current.children[1]));;
+        createLinks(word).then(result => {
+            // create a collapse container
+            const collapseContainer = document.createElement("div");
+            collapseContainer.classList.add("ve-source-link-collapse");
+
+
+            const collapseLink = document.createElement("a");
+            collapseLink.classList.add("ve-source-link-collapse-link", "ve-source-link-collapse-button", "ve-vocab-symbol");
+            collapseLink.textContent = "🔗";
+            
+            const collapseButton = document.createElement("a");
+            collapseButton.classList.add("ve-source-link-collapse-button", "ve-vocab-symbol");
+            collapseButton.textContent = "▶️";
+            collapseContainer.appendChild(result);
+
+            // const collapseButtonWrapper = document.createElement("div");
+            // collapseButtonWrapper.classList.add("ve-source-link-collapse-button-wrapper");
+            // collapseButtonWrapper.appendChild(collapseButton);
+
+
+            collapseButton.addEventListener("click", () => {
+                collapseContainer.classList.toggle("active");
+                result.style.transform = collapseContainer.classList.contains("active") ?
+                    "translateX(0px)" : `translateX(-${result.offsetWidth}px)`;
+                collapseButton.classList.toggle("active");
+            });
+
+            const def = current.querySelector("div.definition");
+            current.insertBefore(collapseLink, def);
+            current.insertBefore(collapseButton, def);
+            current.insertBefore(collapseContainer,def);
+
+            // do transformation after adding
+            // collapseContainer.style.width = `${result.offsetWidth}px`;
+            collapseContainer.style.width = `500px`;
+            result.style.transform = `translateX(-${result.offsetWidth}px)`;
+        });
+
         // disable source links that won't work anyway
         let link = current.querySelector("a.source");
         if (link && link.getAttribute("href") === "https://corpus.vocabulary.com/go/0") {
